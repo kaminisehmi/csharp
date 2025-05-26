80240d3e0cea053403c0ab5896564fd616b5cada
Kamini Sehmi <Kamini.Sehmi@sait.ca>
2025-05-25 20:53:12 -0600

    Add Employee class hierarchy and sample usage

---
+++ ConsoleApp21/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp21
+{
+    class Employee
+    {
+        public string Name { get; set; }
+        public int ID { get; set; }
+        public double Basesalary { get; set; }
+        public Employee(string name, int id, double sal)
+        {
+            Name = name;
+            ID = id;
+            Basesalary = sal;
+        }
+        public virtual double CalculateSalary()
+        {
+            return Basesalary;
+        }
+        public override string ToString()
+        {
+            return $"ID: {ID} and Name: {Name} Basesalary {Basesalary}";
+        }
+        class FullTimeEmployee : Employee
+        {
+            public double Bonus { get; set; }
+            public FullTimeEmployee(string name, int id, double sal, double bonus) : base(name, id, sal)
+            {
+                Bonus = bonus;
+            }
+            public override double CalculateSalary()
+            {
+                return Basesalary + Bonus;
+            }
+            public override string ToString()
+            {
+                // return $"ID: {ID} and Name: {Name} Basesalary {Basesalary} Bonus: {Bonus}";
+                return $"Full time employees details are{base.ToString()},Bonus is :{Bonus},Final salary is: {CalculateSalary()} ";
+            }
+        }
+        class Parttimeemployees : Employee
+        {
+            public double Hourlyrate { get; set; }
+            public int Hoursworked { get; set; }
+            public Parttimeemployees(string name, int id, double sal, double hourlyrate, int hourseworked) : base(name, id, 0)
+            {
+                Hourlyrate = hourlyrate;
+                Hoursworked = hourseworked;
+            }
+            public override double CalculateSalary()
+            {
+                return Hourlyrate * Hoursworked;
+            }
+            public override string ToString()
+            {
+                // return $"ID: {ID} and Name: {Name} Basesalary {Basesalary} Bonus: {Bonus}";
+                return $"Full time employees details are{base.ToString()},Hourlyrate {Hourlyrate},Hourswored{Hoursworked} final salary is: {CalculateSalary()} ";
+            }
+
+
+        }
+        internal class Program
+        {
+            static void Main(string[] args)
+            {
+                List<Employee> employees = new List<Employee>
+                {
+                    new FullTimeEmployee("Alice",101,40000,1000),
+                    new Parttimeemployees("Bob",102,50000,25,100)
+                };
+
+                foreach (var emp in employees)
+                {
+                    Console.WriteLine(emp);
+                }
+            }
+        }
+    }
+}
+    
+
---
+++ ConsoleApp21/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ConsoleApp21")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("SAIT")]
+[assembly: AssemblyProduct("ConsoleApp21")]
+[assembly: AssemblyCopyright("Copyright © SAIT 2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("c5d0f5bd-eb58-4aa6-9135-a4b90a2f94a0")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
