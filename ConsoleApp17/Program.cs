59666cb98dec19bc7201d118bc60f962a46e45d9
Kamini Sehmi <Kamini.Sehmi@sait.ca>
2025-05-26 19:11:27 -0600

    Add Animal, Pig, and Dog classes with polymorphism

---
+++ ConsoleApp17/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+
+namespace ConsoleApp17
+{
+    class Animal
+    {
+        public virtual void animalsound()
+        {
+            Console.WriteLine("The animal makes sound: ");
+        }
+    }
+    class Pig: Animal
+    {
+        public override void animalsound()
+        {
+            Console.WriteLine("This is a pig class: ");
+        }
+    }
+    class Dog : Animal
+    {
+        public override void animalsound()
+        {
+            Console.WriteLine("This is a Dog class: ");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Animal obj1 = new Animal(); //animal object
+            Animal obj2=new Pig();   //pig object
+            Animal obj3= new Dog(); //dog object
+            obj1.animalsound();
+            obj2.animalsound();
+            obj3.animalsound();
+
+        }
+    }
+}
